ecd4a25212d40862afe07fbc1114ee29650014ff
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2017-01-04 09:50:10 -0600

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,55 +1,66 @@
 #!/usr/bin/env bash
-CLION_VERSION=2016.2.1
 
-hello_func() {
-	echo "Hello World"
-	sudo apt-get update
+
+
+vim_func(){
+
+	sudo apt-get install vim -y
 }
 
-clion_func(){
-	sudo wget -q http://download.jetbrains.com/cpp/CLion-${CLION_VERSION}.tar.gz
-	sudo tar xfz CLion-${CLION_VERSION}.tar.gz -C /opt
-
-
+java_func () {
+    sudo apt-get install openjdk-8-jre-headless -y
 }
 
+g++_func(){
 
-#Finally shutdown the system
-shutdown_func(){
-	echo "System Shutting Down"
-	sudo shutdown now -h
-}
-
-
-git_func(){
-
-sudo apt-get install git -y
+	sudo apt-get install gcc g++ -y
 
 }
+git_svn_func(){
 
+	sudo apt-get install git subversion -y
+
+}
 cmake_func(){
 
 	sudo apt-get install cmake -y
 }
 
-g++_func(){
-
-	sudo apt-get install g++ -y
-
-}
 boost_func(){
 
 	sudo apt-get install libboost-all-dev -y
 }
-vim_func(){
 
-	sudo apt-get install vim -y
+
+
+clion_func(){
+	sudo wget -q https://download.jetbrains.com/cpp/CLion-2016.3.2.tar.gz
+	tar xfz CLion-2016.3.2.tar.gz -C /home/vagrant/ 
 }
-#
+
+
+generate_localkeys () {
+     
+   sudo -H -u $1  ssh-keygen -N "" -q -f /home/$1/.ssh/id_generated_rsa
+
+}
+
+
+
+
 vim_func
-hello_func
+echo "installed vim"
+java_func
+echo "installed java"
 g++_func
-git_func
+echo "installed g++"
+git_svn_func
+echo "installed git and svn"
 cmake_func
+echo "installed cmake"
 boost_func
+echo "installed boost libraries"
 clion_func
+echo "installed clion"
+
+generate_localkeys vagrant
